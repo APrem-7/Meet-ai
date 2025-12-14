6cc90ba752554891d70ef29595c716ee9fa22d7e
APrem-7 <ap7leo@gmail.com>
2025-12-15 01:36:51 +0530

    Add: Added the "or continue with" part

--- ai-saas/src/modules/auth/ui/views/sign-in-view.tsx
+++ ai-saas/src/modules/auth/ui/views/sign-in-view.tsx
@@ -93,6 +93,17 @@ export const SignInView = () => {
                     <AlertTitle>Error</AlertTitle>
                   </Alert>
                 )}
+                <Button type="submit" className="w-full">
+                  Sign In
+                </Button>
+                <div
+                  className="after:border-border relative text-center text-sm after:absolute after:inset-0 
+                after:top-1/2 after:z-0 after:flex after:items-center after:border-t"
+                >
+                  <span className="bg-card text-muted-foreground relative z-10 px-2 ">
+                    or continue with
+                  </span>
+                </div>
               </div>
             </form>
           </Form>
