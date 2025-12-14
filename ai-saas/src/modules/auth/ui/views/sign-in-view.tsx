56373489b782addff9f35807f2c3c3e66891c90e
APrem-7 <ap7leo@gmail.com>
2025-12-14 18:22:54 +0530

    Add: implement logo and update sign-in and sign-up views UI

--- ai-saas/src/modules/auth/ui/views/sign-in-view.tsx
+++ ai-saas/src/modules/auth/ui/views/sign-in-view.tsx
@@ -1,4 +1,17 @@
-import { Card } from "@/components/ui/card";
+"use client";
+import { Card, CardContent } from "@/components/ui/card";
 export const SignInView = () => {
-  return <Card>Sign Up Page</Card>;
+  return (
+    <div className="flex flex-col gap-6">
+      <Card className="overflow-hodden p-0">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <form>Col 1</form>
+          <div className="bg-radial from-blue-600 to-blue-700 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+            <img src="/logo.svg" alt="Image" className="h-[104px] w-[92px]" />
+            <p className="text-2xl font-semibold text-white">Zap.AI</p>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
--- ai-saas/src/modules/auth/ui/views/sign-up-view.tsx
+++ ai-saas/src/modules/auth/ui/views/sign-up-view.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Card } from "@/components/ui/card";
 export const SignUpView = () => {
   return <Card>Sign Up Page</Card>;
