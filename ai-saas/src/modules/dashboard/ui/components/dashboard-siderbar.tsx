0808bceddca8829c6b5b04cde32d9b704cc029ea
APrem-7 <ap7leo@gmail.com>
2025-12-17 01:00:39 +0530

    Add: Added dashboard sidebar with Meetings and Agents button

--- ai-saas/src/modules/dashboard/ui/components/dashboard-siderbar.tsx
+++ ai-saas/src/modules/dashboard/ui/components/dashboard-siderbar.tsx
@@ -15,6 +15,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { Separator } from "@/components/ui/separator";
 
 import Link from "next/link";
 import Image from "next/image";
@@ -50,9 +51,32 @@ export const DashboardSidebar = () => {
     <Sidebar>
       <SidebarHeader className="text-sidebar-accent-foreground">
         <Link href="/" className="flex items-center gap-2 px-2 pt-2">
-          <Image src="/logo.svg" height={36} width={36} alt="Meet-ai" />
+          <Image src="/logo.svg" height={36} width={36} alt="ZapAi" />
+          <p className="text-2xl font-semibold ">ZapAi</p>
         </Link>
       </SidebarHeader>
+      <div className="px-4 py-2">
+        <Separator className="opactity-10 text-[#5D6B68]" />
+      </div>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {firstSection.map((item) => (
+                <SidebarMenuItem key={item.href}>
+                  <SidebarMenuButton>
+                    <Link href={item.href}>
+                      <span className="text-sm font-medium tracking-tight">
+                        {item.label}
+                      </span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
     </Sidebar>
   );
 };
