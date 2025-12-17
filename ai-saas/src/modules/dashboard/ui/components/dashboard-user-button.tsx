004dafa0d69cc7dc90026e68b5b20c20e3d3c49a
APrem-7 <ap7leo@gmail.com>
2025-12-17 19:36:09 +0530

    feat: implement DashboardUserButton and displaying the image of the user

--- ai-saas/src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ ai-saas/src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -1,3 +1,28 @@
+import { authClient } from "@/lib/auth-client";
+
+import {
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarImage } from "@/components/ui/avatar";
+
 export const DashboardUserButton = () => {
-  return <div>User Button</div>;
+  const { data, isPending } = authClient.useSession();
+  if (isPending || !data?.user) {
+    return null;
+  }
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger className="rounded-lg border border-border/10 p-3 w-full flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden">
+        {data.user.image ? (
+          <Avatar>
+            <AvatarImage src={data.user.image} />
+          </Avatar>
+        ) : null}
+      </DropdownMenuTrigger>
+    </DropdownMenu>
+  );
 };
