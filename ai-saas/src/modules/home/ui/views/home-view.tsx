cdbc01a5d9d6f41824e12081b57d4b340bac8aee
APrem-7 <ap7leo@gmail.com>
2025-12-24 23:53:14 +0530

    feat(home): replace loading text with LoadingState component
    Improve user experience by showing a more informative loading state when fetching session data

--- ai-saas/src/modules/home/ui/views/home-view.tsx
+++ ai-saas/src/modules/home/ui/views/home-view.tsx
@@ -2,17 +2,22 @@
 
 import { Button } from "@/components/ui/button";
 
-
 import { authClient } from "@/lib/auth-client";
 import { useRouter } from "next/navigation";
 import { AgentView } from "@/modules/agents/ui/views/agent-view";
+import { LoadingState } from "@/components/loading-state";
 
 export const HomeView = () => {
   const router = useRouter();
   const { data: session } = authClient.useSession();
 
   if (!session) {
-    return <p>Loading....</p>;
+    return (
+      <LoadingState
+        title="Loading Profile"
+        description="Please wait while we fetch the session"
+      />
+    );
   }
   return (
     <div className="p-4 flex flex-col gap-y-4">
