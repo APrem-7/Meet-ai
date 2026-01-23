96deb214c53443e7b2fda4e55ebb12bd568bed7d
APrem-7 <ap7leo@gmail.com>
2026-01-23 23:23:50 +0530

    feat(meetingsDialog): add new meeting dialog component
    Add a dialog component for creating new meetings with a placeholder for the meeting form.

---
+++ ai-saas/src/modules/meetings/components/new-meeting-dialog.tsx
@@ -0,0 +1,21 @@
+import { ResponsiveDialog } from '@/components/responsive-dialog';
+interface NewMeetingDialogProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export const NewMeetingDialog = ({open,onOpenChange}:NewMeetingDialogProps)=>{
+    return(
+        <ResponsiveDialog
+            title='New Meeting'
+            description='Create a new meeting'
+            open={open}
+            onOpenChange={onOpenChange}
+        >
+            <div className='py-4'>
+                TODO: Meeting Form
+            </div>
+        </ResponsiveDialog>
+    )
+
+}
